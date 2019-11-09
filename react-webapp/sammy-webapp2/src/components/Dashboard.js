84c4cd488b580a2b92571b3c0579f0c6e50d7d33
Sammy Faraj <sf297@njit.edu>
2019-11-09 18:31:38 -0500

    updated images

--- react-webapp/sammy-webapp2/src/components/Dashboard.js
+++ react-webapp/sammy-webapp2/src/components/Dashboard.js
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 import "../styles/App.css";
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 export default function Dashboard(props) {
     return (
-     <Jumbotron fluid>
-        <Container fluid="true">
-            <h1>Recipe Name</h1>
-            <br></br>
-        </Container>
-    </Jumbotron>
+    <div className='StepBox'>
+        <Jumbotron fluid>
+            <Container fluid="true">
+                <h1>Recipe Name</h1>
+                <h1>1.</h1>
+                <h1>2.</h1>
+                <h1>3.</h1>
+                <h1>4.</h1>
+                <br></br>
+            </Container>
+        </Jumbotron>
+        <Button variant="success" size="lg" block>
+            Different Recipe!
+        </Button>
+    </div>
     )
   }
